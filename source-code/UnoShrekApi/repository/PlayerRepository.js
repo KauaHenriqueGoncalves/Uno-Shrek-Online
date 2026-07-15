bc217c5ecd346a0ed73b7e97716dbf4a3c7f6d5e
KauaHenriqueGoncalves <kauahenriquegoncalves00@gmail.com>
2026-07-15 15:25:34 -0300

    add: continuidade no crud do Player para ficar como exemplo

--- source-code/UnoShrekApi/repository/PlayerRepository.js
+++ source-code/UnoShrekApi/repository/PlayerRepository.js
@@ -1,5 +1,44 @@
+import mongoose from "mongoose";
+
 export default class PlayerRepository {
   constructor(schema) {
     this.schema = schema;
+  }
+
+  async getById(id, session = null) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return null;
+    }
+    return await this.schema.findOne({ _id: id }).session(session);
+  }
+
+  async getByEmail(email, session = null) {
+    return await this.schema.findOne({ email }).session(session);
+  }
+
+  async create(data, session = null) {
+    return await this.schema.create([data], { session });
+  }
+
+  async update(id, data, session = null) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return null;
+    }
+    return await this.schema.findOneAndUpdate({ _id: id }, data, {
+      new: true,
+      session,
+    });
+  }
+
+  async deleteById(id, session = null) {
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return null;
+    }
+    return await this.schema.findOneAndDelete(
+      { _id: id },
+      {
+        session,
+      },
+    );
   }
 }
--- source-code/UnoShrekApi/service/PlayerService.js
+++ source-code/UnoShrekApi/service/PlayerService.js
@@ -1,7 +1,51 @@
 import PlayerRepository from "./../repository/PlayerRepository.js";
+import { NotFoundError } from "../config/exceptions/NotFoundError.js";
+import { BusinessError } from "../config/exceptions/BusinessError.js";
+import { z } from "zod";
+import mongoose from "mongoose";
+import { IlegalInputError } from "../config/exceptions/IlegalInputError.js";
 
 export default class PlayerService {
   constructor(schema) {
     this.playerRepository = new PlayerRepository(schema);
+  }
+
+  //
+  //  TODO: MELHORAR O TRATAMENTO DO INPUT
+  //
+
+  static playerValidation = z.object({
+    name: z.string().trim().min(3).max(50),
+    age: z.number().int().min(1).max(120),
+    email: z.email(),
+  });
+
+  async getById(id) {
+    const player = await this.playerRepository.getById(id);
+    if (!player) {
+      throw new NotFoundError("Player not found");
+    }
+    return player;
+  }
+
+  async create(data) {
+    this.validatePlayer(data);
+    const isExistEmail = await this.playerRepository.getByEmail(data.email);
+    if (isExistEmail) {
+      throw new BusinessError("Email already exists");
+    }
+    return await this.playerRepository.create(data);
+  }
+
+  async update(id, data) {}
+
+  async deleteById(id) {}
+
+  validatePlayer(data) {
+    const result = PlayerService.playerValidation.safeParse(data);
+    if (!result.success) {
+      throw new IlegalInputError("Player input is not valid");
+    }
+    return result.data;
   }
 }
