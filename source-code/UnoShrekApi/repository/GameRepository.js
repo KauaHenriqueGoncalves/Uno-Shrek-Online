e3a7ee65c7a3d7c8a081a1988bcd1dcbc2ba5fe4
davicorreia_dev <davi.correia0125@jala.university>
2026-07-18 20:09:01 -0300

    feat(game): adicionar GameRepository com consulta findByStatus

---
+++ source-code/UnoShrekApi/repository/GameRepository.js
@@ -0,0 +1,23 @@
+import CrudRepository from "./CrudRepository.js";
+import mongoose from "mongoose";
+
+export default class GameRepository extends CrudRepository {
+    constructor(schema) {
+        super(schema);
+    }
+
+    async findByStatus(status, { session = null, limit = 0 } = {}) {
+        // Usamos find para retornar todos que correspondem ao critério
+        const query = this.model.find({ status });
+
+        if (session) {
+            query.session(session);
+        }
+
+        if (limit > 0) {
+            query.limit(limit);
+        }
+
+        return await query.exec(); // Para usar o Promise Nativo
+    }
+}
