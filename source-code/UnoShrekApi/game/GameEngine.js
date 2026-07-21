16ec9446cc13c89e7284a25e0881b9a31a0e1218
Pedro Lucas Farias de Melo <pedro.farias0125@jala.university>
2026-07-21 07:52:17 -0300

    chore: resolve conflicts

---
+++ source-code/UnoShrekApi/game/GameEngine.js
@@ -0,0 +1,16 @@
+import {
+    createDeck,
+    deal,
+    isValidPlay,
+    shuffle,
+    cardEffect,
+    WILD_TYPES,
+    COLORS,
+} from "./deck.js";
+
+const HAND_SIZE = 7;
+
+export default class GameEngine {
+
+
+}
