982ee6628891244f1e7e5ec026d69e8076a8afc0
Pedro Lucas Farias de Melo <pedro.farias0125@jala.university>
2026-07-22 19:06:09 -0300

    feat: refactor card request DTOs and implement CardRepository for improved data handling

--- source-code/UnoShrekApi/dtos/request/card/CreateCardRequestDto.js
+++ source-code/UnoShrekApi/dtos/request/card/CreateCardRequestDto.js
@@ -1,9 +1,10 @@
 import { z } from "zod";
+import { objectIdMongo } from "../../config/utils/validate.js";
  
 const COLORS = ["red", "blue", "green", "yellow", "wild"];
  
 export const CreateCardRequestDto = z.object({
   color: z.enum(COLORS, { message: "color must be: red, blue, green, yellow or wild" }),
   value: z.string().min(1, { message: "value is required" }),
-  gameId: z.string().min(1, { message: "gameId is required" }),
+  gameId: objectIdMongo,
 });
---
+++ source-code/UnoShrekApi/repository/CardRepository.js
@@ -0,0 +1,8 @@
+import CrudRepository from "./CrudRepository.js";
+ 
+export default class CardRepository extends CrudRepository {
+  constructor(schema) {
+    super(schema);
+  }
+}
+ 
