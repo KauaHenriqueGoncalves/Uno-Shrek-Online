444456e273fb164e9ec9b7d06b68682aa0425f1b
KauaHenriqueGoncalves <kauahenriquegoncalves00@gmail.com>
2026-07-22 21:03:53 -0300

    reorganização de pastas e validando alguns métodos

--- source-code/UnoShrekApi/src/dtos/request/game/CreateGameRequestDto.js
+++ source-code/UnoShrekApi/src/dtos/request/game/CreateGameRequestDto.js
@@ -1,8 +1,6 @@
 import { z } from "zod";
 
-const baseFields = {
+export const CreateGameRequestDto = z.object({
   title: z.string().trim().min(3).max(60),
   maxPlayers: z.number().int().min(1).max(4),
-};
-
-export const CreateGameRequestDto = z.object(baseFields);
+});
