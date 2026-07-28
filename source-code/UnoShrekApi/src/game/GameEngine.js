a8e25be70133a820871debe5f22133425672cb0a
KauaHenriqueGoncalves <kauahenriquegoncalves00@gmail.com>
2026-07-28 18:35:20 -0300

    add

--- source-code/UnoShrekApi/src/game/GameEngine.js
+++ source-code/UnoShrekApi/src/game/GameEngine.js
@@ -1,16 +1,13 @@
 import {
-    createDeck,
-    deal,
-    isValidPlay,
-    shuffle,
-    cardEffect,
-    WILD_TYPES,
-    COLORS,
+  createDeck,
+  deal,
+  isValidPlay,
+  shuffle,
+  cardEffect,
+  WILD_TYPES,
+  COLORS,
 } from "./deck.js";
 
 const HAND_SIZE = 7;
 
-export default class GameEngine {
-
-
-}
+export default class GameEngine {}
