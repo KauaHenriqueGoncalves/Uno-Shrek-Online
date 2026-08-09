f881b5391450d049102dcdfc249ebd2a852508df
KauaHenriqueGoncalves <kauahenriquegoncalves00@gmail.com>
2026-08-09 16:08:50 -0300

    feat: adionado mensagem para os jogadores da sala

--- source-code/UnoShrekApi/src/modules/sockets/events/player.events.js
+++ source-code/UnoShrekApi/src/modules/sockets/events/player.events.js
@@ -1,9 +1,11 @@
 const PLAYER_EVENTS = {
   INPUT: {
     GET_ONLINE_COUNT: "player::getOnlineCount",
+    MESSAGE_ROOM:     "player::messageRoom",
   },
   OUTPUT: {
     ONLINE_COUNT:     "player::onlineCount",
+    MESSAGE_ROOM_OUT: "player::messageRoomOut",
     ERROR:            "player::error",
   },
 };
