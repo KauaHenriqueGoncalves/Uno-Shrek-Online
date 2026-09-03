f93c4ced550c3991f0b3177753c4635fb1323893
KauaHenriqueGoncalves <kauahenriquegoncalves00@gmail.com>
2026-09-03 13:57:36 -0300

    feat: adicionando reações durante o jogo

---
+++ source-code/UnoShrekApi/src/modules/sockets/events/emoji.event.js
@@ -0,0 +1,11 @@
+const EMOJI_EVENTS = {
+  INPUT: {
+    SEND_EMOJI: "emoji::send",
+  },
+  OUTPUT: {
+    EMOJI_SENT: "emoji::sent",
+    EMOJI_ERROR: "emoji::error",
+  },
+};
+
+export default EMOJI_EVENTS;
